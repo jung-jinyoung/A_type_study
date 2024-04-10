bbef31153919ec2d3ee7a1035bfcc3447494c17e
bokbok1997 <156386786+bokbok1997@users.noreply.github.com>
2024-04-10 10:11:38 +0900

    Add files via upload

--- 240410/SWEA/2115_이현복.py
+++ 240410/SWEA/2115_이현복.py
@@ -3,53 +3,64 @@ sys.stdin = open('sample_input.txt','r')
 
 
 '''
-2115_벌꿀채취
-237 ms
-52,456 kb
-'''
 
-def dfs(level,sumv):
+
+
+'''
+road=((1,1),(-1,1),(-1,-1),(1,-1))
+
+def dfs(x,y,dir,visited,todo,static):
     global res
-    if level == 2:          # 2명이니까 종료는 2일때
-        if res < sumv:      # 더 클때 갱신
-            res = sumv
+    if dir ==4:
+        # if visited.count(1) == 10:
+        #     for i in range(len(visited)):
+        #         if visited[i]: print(i,end=" ")
+
+        # print(static,visited.count(1),visited)
+        res = max(res, visited.count(1))
         return
-    for i in range(N):
-        for j in range(nn):
-            for l in range(j-M,j+M):
-                if 0<=l<nn:                 # 앞뒤로 겹치는 구간
-                    if visited[i][l]!=0:    # 방문 했으면 불가한 경우니까 break
-                        break
-            else:                           # for-else로 앞뒤 곂치는 구간 없다
-                if visited[i][j] == 0:      # 방문한 적 없으면
-                    visited[i][j] = 1       # 방문 표시
-                    dfs(level+1,sumv+data[i][j])
-                    visited[i][j] = 0       # 방문 취소
+    if dir < 2:
+        # for i in range(1,N-1):
+        ni,nj = x+road[dir][0],y+road[dir][1]
+        if 0<=ni<N and 0<=nj<N and not visited[arr[ni][nj]]:
+            # print(ni,nj)
+            visited[arr[ni][nj]]=1
+            todo[dir]+=1
+            dfs(ni, nj, dir, visited,todo,static)
+            dfs(ni, nj, dir+1, visited,todo,static)
+            return
+        else:
+            return
+    else:
+        # for i in range(1,N-1):
+        # if 1:
+        ni,nj = x+road[dir][0],y+road[dir][1]
+        if 0<=ni<N and 0<=nj<N and (visited[arr[ni][nj]]==0 or visited[arr[ni][nj]]==10):
+            # print(ni, nj)
+            visited[arr[ni][nj]]=1
+            todo[dir-2]-=1
+            if todo[dir-2]==0:
+                dfs(ni, nj, dir + 1, visited,todo,static)
+            else:
+                dfs(ni, nj, dir, visited,todo,static)
+        else:
+            return
+
+
+
 
 T = int(input())
 for tc in range(T):
-    N,M,C = map(int,input().split())
+    N = int(input())
     arr = [list(map(int,input().split())) for _ in range(N)]
-    nn = N-M+1                              # M개씩 그룹화 했을때 수익을 2중 리스트로 만듬
-    data = [[0]*nn for _ in range(N)]
-    for i in range(N):
-        for j in range(N - M + 1):
-            temp = []
-            for k in range(M):
-                temp.append(arr[i][j+k])        # M개를 append
-            if sum(temp)<=C:                    # M개의 합을 C와 비교
-                data[i][j]=sum(map(lambda x:x**2,temp))     # 각 원소 제곱하고 합친다
-            else:
-                comp = 0
-                for x in range(1<<M):       # 조합으로 합이 C보다 작고 제곱 합이 최대 인거 찾는다.
-                    stack = []
-                    for y in range(M):
-                        if x&(1<<y):
-                            stack.append(temp[y])
-                    if sum(stack)<=C:
-                        comp = max(comp,sum(map(lambda x:x**2,stack)))
-                data[i][j] = comp
-    visited = [[0]*nn for _ in range(N)]
-    res=0
-    dfs(0,0)
-    print(f'#{tc+1} {res}')
+    if 1:
+        res = -1
+        for i in range(1,N-1):
+            for j in range(N-2):
+                check=[0]*101
+                check[arr[i][j]]=10
+                dfs(i,j,0,check,[0,0],[0,0])
+        # check=[0]*101
+        # check[arr[1][2]]=10
+        # dfs(1,2,0,check,[0,0])
+        print(f'#{1+tc} {res}')
